0f76f1c56c2030815e9ffca3348a31b8bade3b45
Enceka <enceka@yeah.net>
2024-11-12 12:29:29 +0800

    Update thermal@2.0

--- aidl/thermal/Thermal.h
+++ aidl/thermal/Thermal.h
@@ -1,133 +1,98 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #pragma once
 
-#include <aidl/android/hardware/thermal/BnThermal.h>
+#include <android/hardware/thermal/2.0/IThermal.h>
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <hardware/thermal.h>
+#include "thermal_helper.h"
 
-#include <mutex>
-#include <thread>
-
-#include "thermal-helper.h"
-
-namespace aidl {
 namespace android {
 namespace hardware {
 namespace thermal {
+namespace V2_0 {
 namespace implementation {
 
+using ::android::sp;
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::thermal::V1_0::CpuUsage;
+using ::android::hardware::thermal::V2_0::CoolingType;
+using ::android::hardware::thermal::V2_0::IThermal;
+using CoolingDevice_1_0 = ::android::hardware::thermal::V1_0::CoolingDevice;
+using CoolingDevice_2_0 = ::android::hardware::thermal::V2_0::CoolingDevice;
+using Temperature_1_0 = ::android::hardware::thermal::V1_0::Temperature;
+using Temperature_2_0 = ::android::hardware::thermal::V2_0::Temperature;
+using ::android::hardware::thermal::V2_0::IThermalChangedCallback;
+using ::android::hardware::thermal::V2_0::TemperatureThreshold;
+using ::android::hardware::thermal::V2_0::TemperatureType;
+
+
 struct CallbackSetting {
-    CallbackSetting(std::shared_ptr<IThermalChangedCallback> callback, bool is_filter_type,
-                    TemperatureType type)
-        : callback(std::move(callback)), is_filter_type(is_filter_type), type(type) {}
-    std::shared_ptr<IThermalChangedCallback> callback;
-    bool is_filter_type;
-    TemperatureType type;
+	CallbackSetting(sp<IThermalChangedCallback> callback, bool is_filter_type, TemperatureType type)
+		: callback(callback), is_filter_type(is_filter_type), type(type) {}
+	sp<IThermalChangedCallback> callback;
+	bool is_filter_type;
+	TemperatureType type;
 };
 
-struct CoolingDeviceCallbackSetting {
-    CoolingDeviceCallbackSetting(std::shared_ptr<ICoolingDeviceChangedCallback> callback,
-                                 bool is_filter_type, CoolingType type)
-        : callback(std::move(callback)), is_filter_type(is_filter_type), type(type) {}
-    std::shared_ptr<ICoolingDeviceChangedCallback> callback;
-    bool is_filter_type;
-    CoolingType type;
-};
+class Thermal : public IThermal {
 
-class Thermal : public BnThermal {
   public:
-    Thermal();
-    explicit Thermal(const std::shared_ptr<ThermalHelper> &helper);
-    ~Thermal() = default;
-    ndk::ScopedAStatus getTemperatures(std::vector<Temperature> *_aidl_return) override;
-    ndk::ScopedAStatus getTemperaturesWithType(TemperatureType type,
-                                               std::vector<Temperature> *_aidl_return) override;
+	//Thermal(thermal_module_t* module);
+	Thermal();
+	~Thermal() = default;
 
-    ndk::ScopedAStatus getTemperatureThresholds(
-            std::vector<TemperatureThreshold> *_aidl_return) override;
-    ndk::ScopedAStatus getTemperatureThresholdsWithType(
-            TemperatureType type, std::vector<TemperatureThreshold> *_aidl_return) override;
+	// Methods from ::android::hardware::thermal::V1_0::IThermal follow.
+	Return<void> getTemperatures(getTemperatures_cb _hidl_cb) override;
+	Return<void> getCpuUsages(getCpuUsages_cb _hidl_cb) override;
+	Return<void> getCoolingDevices(getCoolingDevices_cb _hidl_cb) override;
 
-    ndk::ScopedAStatus registerThermalChangedCallback(
-            const std::shared_ptr<IThermalChangedCallback> &callback) override;
-    ndk::ScopedAStatus registerThermalChangedCallbackWithType(
-            const std::shared_ptr<IThermalChangedCallback> &callback,
-            TemperatureType type) override;
-    ndk::ScopedAStatus unregisterThermalChangedCallback(
-            const std::shared_ptr<IThermalChangedCallback> &callback) override;
-
-    ndk::ScopedAStatus getCoolingDevices(std::vector<CoolingDevice> *_aidl_return) override;
-    ndk::ScopedAStatus getCoolingDevicesWithType(CoolingType type,
-                                                 std::vector<CoolingDevice> *_aidl_return) override;
-
-    ndk::ScopedAStatus registerCoolingDeviceChangedCallbackWithType(
-            const std::shared_ptr<ICoolingDeviceChangedCallback> &callback,
-            CoolingType type) override;
-    ndk::ScopedAStatus unregisterCoolingDeviceChangedCallback(
-            const std::shared_ptr<ICoolingDeviceChangedCallback> &callback) override;
-
-    binder_status_t dump(int fd, const char **args, uint32_t numArgs) override;
+	// Methods from ::android::hardware::thermal::V2_0::IThermal follow.
+	Return<void> getCurrentTemperatures(bool filterType, TemperatureType type,
+							getCurrentTemperatures_cb _hidl_cb) override;
+	Return<void> getTemperatureThresholds(bool filterType, TemperatureType type,
+							getTemperatureThresholds_cb _hidl_cb) override;
+	Return<void> registerThermalChangedCallback(
+		const sp<IThermalChangedCallback>& callback, bool filterType, TemperatureType type,
+		registerThermalChangedCallback_cb _hidl_cb) override;
+	Return<void> unregisterThermalChangedCallback(
+		const sp<IThermalChangedCallback>& callback,
+		unregisterThermalChangedCallback_cb _hidl_cb) override;
+	Return<void> getCurrentCoolingDevices(bool filterType, CoolingType type,
+							getCurrentCoolingDevices_cb _hidl_cb) override;
 
     // Helper function for calling callbacks
-    void sendThermalChangedCallback(const Temperature &t);
-
+	void sendThermalChangedCallback(const std::vector<Temperature_2_0> &temps);
   private:
-    class Looper {
-      public:
-        struct Event {
-            std::function<void()> handler;
-        };
-
-        Looper() {
-            thread_ = std::thread([&] { loop(); });
-        }
-        ~Looper();
-
-        void addEvent(const Event &e);
-
-      private:
-        std::condition_variable cv_;
-        std::queue<Event> events_;
-        std::mutex mutex_;
-        std::thread thread_;
-        bool aborted_;
-
-        void loop();
-    };
-
-    std::shared_ptr<ThermalHelper> thermal_helper_;
-    std::mutex thermal_callback_mutex_;
-    std::vector<CallbackSetting> callbacks_;
-    std::mutex cdev_callback_mutex_;
-    std::vector<CoolingDeviceCallbackSetting> cdev_callbacks_;
-
-    Looper looper_;
-
-    ndk::ScopedAStatus getFilteredTemperatures(bool filterType, TemperatureType type,
-                                               std::vector<Temperature> *_aidl_return);
-    ndk::ScopedAStatus getFilteredCoolingDevices(bool filterType, CoolingType type,
-                                                 std::vector<CoolingDevice> *_aidl_return);
-    ndk::ScopedAStatus getFilteredTemperatureThresholds(
-            bool filterType, TemperatureType type, std::vector<TemperatureThreshold> *_aidl_return);
-    ndk::ScopedAStatus registerThermalChangedCallback(
-            const std::shared_ptr<IThermalChangedCallback> &callback, bool filterType,
-            TemperatureType type);
-
-    void dumpVirtualSensorInfo(std::ostringstream *dump_buf);
-    void dumpThrottlingInfo(std::ostringstream *dump_buf);
-    void dumpThrottlingRequestStatus(std::ostringstream *dump_buf);
-    void dumpPowerRailInfo(std::ostringstream *dump_buf);
-    void dumpStatsRecord(std::ostringstream *dump_buf, const StatsRecord &stats_record,
-                         std::string_view line_prefix);
-    void dumpThermalStats(std::ostringstream *dump_buf);
-    void dumpThermalData(int fd);
+	ThermalHelper thermal_helper_;
+	std::mutex thermal_callback_mutex_;
+	std::vector<CallbackSetting> callbacks_;
 };
+extern "C" IThermal* HIDL_FETCH_IThermal(const char* name);
+
 
 }  // namespace implementation
+}  // namespace V2_0
 }  // namespace thermal
 }  // namespace hardware
 }  // namespace android
-}  // namespace aidl
--- aidl/thermal/service.cpp
+++ aidl/thermal/service.cpp
@@ -1,41 +1,39 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+#define LOG_TAG "android.hardware.thermal@2.0-service"
+
 #include <android-base/logging.h>
-#include <android/binder_manager.h>
-#include <android/binder_process.h>
-
+#include <hidl/HidlTransportSupport.h>
 #include "Thermal.h"
-
-constexpr std::string_view kThermalLogTag("pixel-thermal");
+#include <android/hardware/thermal/1.0/IThermal.h>
+#include <hidl/LegacySupport.h>
 
 using ::android::OK;
 using ::android::status_t;
 
-// Generated AIDL files:
-using Thermal = ::aidl::android::hardware::thermal::implementation::Thermal;
+// libhwbinder:
+using ::android::hardware::configureRpcThreadpool;
+using ::android::hardware::joinRpcThreadpool;
 
-#if !defined(THERMAL_INSTANCE_NAME)
-#define THERMAL_INSTANCE_NAME "default"
-#endif
+// Generated HIDL files:
+using ::android::hardware::thermal::V2_0::IThermal;
+using ::android::hardware::thermal::V2_0::implementation::Thermal;
+using android::hardware::defaultPassthroughServiceImplementation;
 
-int main(int /* argc */, char ** /* argv */) {
-    android::base::SetDefaultTag(kThermalLogTag.data());
-
-    auto svc = ndk::SharedRefBase::make<Thermal>();
-    const auto svcName = std::string() + svc->descriptor + "/" + THERMAL_INSTANCE_NAME;
-    LOG(INFO) << "Pixel Thermal AIDL Service starting..." + svcName;
-    ABinderProcess_setThreadPoolMaxThreadCount(0);
-
-    auto svcBinder = svc->asBinder();
-    binder_status_t status = AServiceManager_addService(svcBinder.get(), svcName.c_str());
-    if (status != STATUS_OK) {
-        LOG(ERROR) << "Pixel Thermal AIDL Service failed to start: " << status << ".";
-        return EXIT_FAILURE;
-    }
-    LOG(INFO) << "Pixel Thermal HAL AIDL Service started.";
-    ABinderProcess_joinThreadPool();
-    return EXIT_FAILURE;  // should not reach
+int main() {
+	return defaultPassthroughServiceImplementation<IThermal>();
 }
